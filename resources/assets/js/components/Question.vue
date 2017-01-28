699f73cb87404cf37365ab9c42fe0b9114c53d37
Julien Tant <julien@craftyx.fr>
2017-01-28 13:06:49 +0100

    Frontend : use button to answer to question #4

--- resources/assets/js/components/Question.vue
+++ resources/assets/js/components/Question.vue
@@ -2,42 +2,40 @@
     <div class="panel panel-default">
         <div class="panel-body">
 
-            <h2>{{ question.category.name }}</h2>
+            <div class="row">
+                <div class="col-md-6">
+                    <h2>{{ question.category.name }}</h2>
+                </div>
+                <div class="col-md-6 text-right">
+                    <template v-if="!!question.question.page_no">
+                        <strong>Page {{ question.question.page_no }}</strong>
+                    </template>
+                </div>
+            </div>
 
-            <template v-if="!!question.question.page_no">
-                <strong>Page {{ question.question.page_no }}</strong>
-            </template>
 
-            <p v-html="formattedProposition"></p>
+            <p class="proposition" v-html="formattedProposition"></p>
 
-            <p v-if="formatedDescription != ''" v-html="formatedDescription"></p>
+            <p class="description" v-if="formatedDescription != ''" v-html="formatedDescription"></p>
 
             <div class="row text-center">
-                <div class="col-md-2">
-                    <label class="radio-inline">
-                        <input v-model="question.answer" type="radio" :value="0" :name="'rep_' + question.id"/> 0
-                    </label>
-                </div>
-                <div class="col-md-2">
-                    <label class="radio-inline">
-                        <input v-model="question.answer" type="radio" :value="25" :name="'rep_' + question.id"/> 25
-                    </label>
-                </div>
-                <div class="col-md-2">
-                    <label class="radio-inline">
-                        <input v-model="question.answer" type="radio" :value="50" :name="'rep_' + question.id"/> 50
-                    </label>
-                </div>
-                <div class="col-md-2">
-                    <label class="radio-inline">
-                        <input v-model="question.answer" type="radio" :value="75" :name="'rep_' + question.id"/> 75
-                    </label>
-                </div>
-                <div class="col-md-2">
-                    <label class="radio-inline">
-                        <input v-model="question.answer" type="radio" :value="100" :name="'rep_' + question.id"/> 100
-                    </label>
-                </div>
+                <button @click="selectAnswer(0, $event)" class="btn"
+                        :class="{'btn-success': question.answer==0, 'btn-default': question.answer!=0}">Pas d'accord
+                </button>
+                <button @click="selectAnswer(25, $event)" class="btn"
+                        :class="{'btn-success': question.answer==25, 'btn-default': question.answer!=25}">Plutôt pas
+                    d'accord
+                </button>
+                <button @click="selectAnswer(50, $event)" class="btn"
+                        :class="{'btn-success': question.answer==50, 'btn-default': question.answer!=50}">Neutre
+                </button>
+                <button @click="selectAnswer(75, $event)" class="btn"
+                        :class="{'btn-success': question.answer==75, 'btn-default': question.answer!=75}">Plutôt
+                    d'accord
+                </button>
+                <button @click="selectAnswer(100, $event)" class="btn"
+                        :class="{'btn-success': question.answer==100, 'btn-default': question.answer!=100}">D'accord
+                </button>
             </div>
 
         </div>
@@ -58,6 +56,27 @@
                 return (this.question.question.description + '').replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1' + '<br />' + '$2')
             },
         },
+        methods: {
+            selectAnswer(value, $event) {
+                this.question.answer = value
+                $event.target.blur();
+            }
+        },
         props: ['question'],
     }
 </script>
+
+
+<style scoped>
+    h2 {
+        margin-top: 0;
+    }
+
+    p.proposition {
+        font-weight: bold;
+    }
+
+    p.description {
+        font-style: italic;
+    }
+</style>
