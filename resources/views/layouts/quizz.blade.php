dccdd6fc165473b8061cb6987750a53f9f1356a0
DarckCrystale <crystale.darck@gmail.com>
2017-02-12 20:47:22 +0100

    Ref #3: mise en page de l'accueil - joli tout beau - pied de page (mais ya pas les liens à jour encore).

--- resources/views/layouts/quizz.blade.php
+++ resources/views/layouts/quizz.blade.php
@@ -23,8 +23,45 @@
 <body>
     <div id="app">
         @yield('content')
-    </div>
 
+        <div class="container-fluid">  
+            <div class="col-lg-4 col-lg-offset-4 col-md-4 col-md-offset-4 col-sm-4 col-sm-offset-4 col-xs-6 col-xs-offset-3">
+                <div class="fi-createurs col-lg-8 col-lg-offset-2">
+                    <div class="fi-createurs-border">Site créé à l'initiative<br/>de citoyen.ne.s insoumis.es</div>
+                </div>
+            </div>
+        </div>
+        <div class="container-fluid">
+            <div class="fi-footer col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1 col-sm-12 col-xs-12">
+                <div class="col-lg-3 col-md-3 col-sm-3 col-xs-6">
+                    <p class="fi-footer-titre">Je m'implique&nbsp;:</p>
+                    <p><a href="">J'agis</a></p>
+                    <p><a href="">Je donne</a></p>
+                    <p><a href="">Les parrainages</a></p>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-3 col-xs-6">
+                    <p class="fi-footer-titre">Réseaux sociaux&nbsp;:</p>
+                    <p><a href="">Facebook</a></p>
+                    <p><a href="">Twitter</a></p>
+                    <p><a href="">YouTube</a></p>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-3 col-xs-6">
+                    <p class="fi-footer-titre">Groupes d'appui&nbsp;:</p>
+                    <p><a href="">La carte des groupes</a></p>
+                    <p><a href="">L'actu des groupes</a></p>
+                    <p><a href="">Créer ou rejoindre un groupe</a></p>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-3 col-xs-6">
+                    <p class="fi-footer-titre">Événements&nbsp;:</p>
+                    <p><a href="">L'agenda de JLM</a></p>
+                    <p><a href="">Événements locaux</a></p>
+                </div>
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 fi-footer-jlm2017">
+                    <a href="http://www.jlm2017.fr">jlm2017.fr</a>
+                </div>
+            </div>
+        </div>
+    </div>
     <!-- Scripts -->
     <script src="{{ mix("/js/app.js") }}"></script>
 </body>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,60 +1,90 @@
 @extends('layouts.quizz')
 
 @section('content')
-    <div class="container">
+    <div class="container-fluid">
         <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                        <div class="pull-right">
-                            <a class="btn btn-success btn-sm" href="{{ route('top') }}">
-                                <i class="fa fa-trophy"></i>
-                                Top des propositions
-                            </a>
-                        </div>
-
-                        <strong>Quizz politique sur la France insoumise</strong>
+            <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1 fi-accueil-bloc">
+                <div class="fi-top-menu">
+                    <a class="fi-facebook" href=""></a>
+                    <a class="fi-twitter" href=""></a>
+                    <a class="fi-youtube" href=""></a>
+                    <a class="btn btn-info btn-sm fi-top-propositions" href="{{ route('top') }}">
+                        <i class="fa fa-trophy"></i>
+                        Top des propositions
+                    </a>
+                </div>
+                <div class="panel panel-default fi-boite-accueil">
+<!--
+                    <div class="panel-heading fi-accueil-titre">
+                        <strong>Suis-je<br/>Insoumis.fr</strong>
                         <div class="clearfix"></div>
                     </div>
+-->
                     <div class="panel-body">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid atque cum cumque
-                            dignissimos dolorum esse est, ipsam maiores, maxime minima molestias nemo perferendis, quasi
-                            ratione sed similique tempora unde vitae.</p>
-
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus harum pariatur tempore
-                            ullam. Aspernatur cupiditate nobis odit quaerat qui quia soluta tenetur. Blanditiis,
-                            expedita illo magnam odio quasi totam voluptatem!</p>
+                        <div class="fi-accueil-titre">
+                            <strong>Suis-je<br/>Insoumis.fr</strong>
+                        </div>
+                        
+                        <p>Découvrez grâce à un tirage aléatoire les propositions du programme de la France Insoumise,<br/>
+                        représentée par son candidat <strong>Jean-Luc MÉLENCHON</strong>.</p>
+                        <p>À l'issue de la série de questions de votre choix, vous saurez si vous adhérez aux idées du mouvement...</p>
+                        <p class="fi-accueil-etonnement"><strong>Le résultat pourrait bien vous étonner&nbsp;!</strong></p>
 
                         <form action="{{ route('generate_quizz') }}" method="post">
                             {{ csrf_field() }}
                             <div class="row">
-                                <div class="col-md-4 text-left">
+                                <div class="col-md-4 col-sm-4 text-left">
                                     <button class="btn btn-block btn-primary" type="submit" name="type" value="10">
-                                        10 propositions<br/>(environ 1 min)
+                                        <div class="fi-contenu-bouton">
+                                            <div class="fi-icon-timer"></div>
+                                            <div class="fi-texte-bouton">
+                                                <span class="fi-bouton-titre">10 propositions</span>
+                                                <br/>
+                                                environ 1 minute
+                                            </div>
+                                        </div>
                                     </button>
                                 </div>
-                                <div class="col-md-4 text-center">
+                                <div class="col-md-4 col-sm-4 text-center">
                                     <button class="btn btn-block btn-primary" type="submit" name="type" value="20">
-                                        20 propositions<br/>(environ 2 min)
+                                        <div class="fi-contenu-bouton">
+                                            <div class="fi-icon-timer"></div>
+                                            <div class="fi-texte-bouton">
+                                                <span class="fi-bouton-titre">20 propositions</span>
+                                                <br/>
+                                                environ 2 minutes
+                                            </div>
+                                        </div>
                                     </button>
                                 </div>
-                                <div class="col-md-4 text-right">
+                                <div class="col-md-4 col-sm-4 text-right">
                                     <button class="btn btn-block btn-primary" type="submit" name="type" value="40">
-                                        40 propositions<br/>(environ 5 min)
+                                        <div class="fi-contenu-bouton">
+                                            <div class="fi-icon-timer"></div>
+                                            <div class="fi-texte-bouton">
+                                                <span class="fi-bouton-titre">40 propositions</span>
+                                                <br/>
+                                                environ 5 minutes
+                                            </div>
+                                        </div>
                                     </button>
                                 </div>
                             </div>
                         </form>
 
+<!--
                         <p>
-                            <h3>Faites tourner !</h3>
-                            <a href="http://www.facebook.com/sharer/sharer.php?u={{ URL::full() }}"><i
-                                        class="fa fa-facebook" aria-hidden="true"></i></a>
-                            <a href="http://twitter.com/intent/tweet?status={{ urlencode('Venez participer au quizz sur la France insoumise et son programme l\'Avenir en commun : ' . URL::full()) }}"><i
-                                        class="fa fa-twitter" aria-hidden="true"></i></a>
-                            <a href="https://plus.google.com/share?url={{ URL::full() }}"><i class="fa fa-google-plus"
-                                                                                             aria-hidden="true"></i></a>
+                            <a href="http://www.facebook.com/sharer/sharer.php?u={{ URL::full() }}">
+                                <i class="fa fa-facebook" aria-hidden="true"></i>
+                            </a>
+                            <a href="http://twitter.com/intent/tweet?status={{ urlencode('Venez participer au quizz sur la France insoumise et son programme l\'Avenir en commun : ' . URL::full()) }}">
+                                <i class="fa fa-twitter" aria-hidden="true"></i>
+                            </a>
+                            <a href="https://plus.google.com/share?url={{ URL::full() }}">
+                                <i class="fa fa-google-plus" aria-hidden="true"></i>
+                            </a>
                         </p>
+-->
                     </div>
                 </div>
             </div>
