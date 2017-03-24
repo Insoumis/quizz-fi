eb521638286eeefb7563d53ac5b6ae0ba74fbb10
Julien Tant <julien@craftyx.fr>
2017-03-24 18:49:26 +0100

    remove useless br

--- resources/views/layouts/quizz.blade.php
+++ resources/views/layouts/quizz.blade.php
@@ -47,10 +47,12 @@
         <div class="container-fluid">
             <div class="col-lg-4 col-lg-offset-4 col-md-4 col-md-offset-4 col-sm-4 col-sm-offset-4 col-xs-6 col-xs-offset-3">
                 <div class="fi-createurs col-lg-8 col-lg-offset-2">
-                    <div class="fi-createurs-border">Site créé à l'initiative<br/>de citoyen.ne.s insoumis.es</div>
+                    <div class="fi-createurs-border">Site créé à l'initiative de citoyen.ne.s insoumis.es</div>
                 </div>
             </div>
         </div>
+
+
         <div class="container-fluid" style="margin-top: 30px; margin-bottom: 80px; ">
             <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
             <div class="row bottom-margin" style="background: rgba(255,255,255,0.85); padding: 1em;">
