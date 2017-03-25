e3743e40d1a5e12215250f837dca5fa598f99bd5
Julien Tant <julien@craftyx.fr>
2017-03-25 10:40:27 +0100

    add link to the credit block #8

--- resources/views/layouts/quizz.blade.php
+++ resources/views/layouts/quizz.blade.php
@@ -9,23 +9,25 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>Suis-je Insoumis ?</title>
-    <meta name="description" content="Découvrez grâce à un tirage aléatoire les propositions du programme de la France Insoumise, représentée par son candidat Jean-Luc MÉLENCHON.">
+    <meta name="description"
+          content="Découvrez grâce à un tirage aléatoire les propositions du programme de la France Insoumise, représentée par son candidat Jean-Luc MÉLENCHON.">
 
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta charset="UTF-8">
 
-    <meta property="og:title" content="Suis-je Insoumis ?" />
-    <meta property="og:type" content="website" />
-    <meta property="og:url" content="https://suisjeinsoumis.fr/" />
-    <meta property="og:description" content="Découvrez grâce à un tirage aléatoire les propositions du programme de la France Insoumise, représentée par son candidat Jean-Luc MÉLENCHON." />
+    <meta property="og:title" content="Suis-je Insoumis ?"/>
+    <meta property="og:type" content="website"/>
+    <meta property="og:url" content="https://suisjeinsoumis.fr/"/>
+    <meta property="og:description"
+          content="Découvrez grâce à un tirage aléatoire les propositions du programme de la France Insoumise, représentée par son candidat Jean-Luc MÉLENCHON."/>
 
-    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="/images/ico/apple-touch-icon-144x144.png" />
-    <link rel="apple-touch-icon-precomposed" sizes="152x152" href="/images/ico/apple-touch-icon-152x152.png" />
-    <link rel="icon" type="image/png" href="/images/ico/favicon-32x32.png" sizes="32x32" />
-    <link rel="icon" type="image/png" href="/images/ico/favicon-16x16.png" sizes="16x16" />
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="/images/ico/apple-touch-icon-144x144.png"/>
+    <link rel="apple-touch-icon-precomposed" sizes="152x152" href="/images/ico/apple-touch-icon-152x152.png"/>
+    <link rel="icon" type="image/png" href="/images/ico/favicon-32x32.png" sizes="32x32"/>
+    <link rel="icon" type="image/png" href="/images/ico/favicon-16x16.png" sizes="16x16"/>
     <meta name="application-name" content="Suis-je Insoumis ?"/>
-    <meta name="msapplication-TileColor" content="#0098B6" />
-    <meta name="msapplication-TileImage" content="/images/ico/mstile-144x144.png" />
+    <meta name="msapplication-TileColor" content="#0098B6"/>
+    <meta name="msapplication-TileImage" content="/images/ico/mstile-144x144.png"/>
     <meta name="theme-color" content="#0098b6">
     <link rel="icon" href="/images/ico/favicon.ico">
 
@@ -41,20 +43,22 @@
     </script>
 </head>
 <body>
-    <div id="app">
-        @yield('content')
+<div id="app">
+    @yield('content')
 
-        <div class="container-fluid">
-            <div class="col-lg-4 col-lg-offset-4 col-md-4 col-md-offset-4 col-sm-4 col-sm-offset-4 col-xs-6 col-xs-offset-3">
+    <div class="container-fluid">
+        <div class="col-lg-4 col-lg-offset-4 col-md-4 col-md-offset-4 col-sm-4 col-sm-offset-4 col-xs-6 col-xs-offset-3">
+            <a href="http://discord.insoumis.online/" class="fi-createurs-link" target="_blank">
                 <div class="fi-createurs">
                     <div class="fi-createurs-border">Site créé à l'initiative de citoyen.ne.s insoumis.es</div>
                 </div>
-            </div>
+            </a>
         </div>
+    </div>
 
 
-        <div class="container-fluid" style="margin-top: 30px; margin-bottom: 80px; ">
-            <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+    <div class="container-fluid" style="margin-top: 30px; margin-bottom: 80px; ">
+        <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
             <div class="row bottom-margin" style="background: rgba(255,255,255,0.85); padding: 1em;">
                 <div class="col-sm-3"><strong>Je m’implique&nbsp;:</strong><br>
                     <a target="_blank" href="http://www.jlm2017.fr/agir">J’agis</a><br>
@@ -74,8 +78,10 @@
 
                 <div class="col-sm-3"><strong>Groupes d’appui&nbsp;:</strong><br>
                     <a target="_blank" href="http://www.jlm2017.fr/groupes_appui">La carte des groupes</a><br>
-                    <a target="_blank" href="http://www.jlm2017.fr/actualites-groupes-appui">L'actu des groupes d'appui</a><br>
-                    <a target="_blank" href="http://www.jlm2017.fr/creer_ou_rejoindre_un_groupe_d_appui">Créer ou rejoindre un groupe</a>
+                    <a target="_blank" href="http://www.jlm2017.fr/actualites-groupes-appui">L'actu des groupes
+                        d'appui</a><br>
+                    <a target="_blank" href="http://www.jlm2017.fr/creer_ou_rejoindre_un_groupe_d_appui">Créer ou
+                        rejoindre un groupe</a>
                 </div>
 
                 <div class="col-sm-3"><strong>Evènements&nbsp;:</strong><br>
@@ -83,11 +89,11 @@
                     <a target="_blank" href="http://www.jlm2017.fr/evenements_locaux">Événements locaux</a><br>
                 </div>
             </div>
-            </div>
         </div>
-
     </div>
-    <!-- Scripts -->
-    <script src="{{ mix("/js/app.js") }}"></script>
+
+</div>
+<!-- Scripts -->
+<script src="{{ mix("/js/app.js") }}"></script>
 </body>
 </html>
