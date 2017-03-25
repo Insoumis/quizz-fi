83379f4d10cebc3ba46662e2a9a82db6e7eb657f
Julien Tant <julien@craftyx.fr>
2017-03-25 12:03:19 +0100

    fix typo

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -30,7 +30,7 @@
                                     {{ $question->proposition }}
                                 </div>
                                 <div class="col-md-3">
-                                    {{ round($result->pct, 2) }}% <i class="fa fa-info-circle" data-toggle="tooltip" title="Score moyen sur une total de {{ $result->total }} affichages"></i>
+                                    {{ round($result->pct, 2) }}% <i class="fa fa-info-circle" data-toggle="tooltip" title="Score moyen sur un total de {{ $result->total }} affichages"></i>
                                 </div>
                                 <div class="col-md-1">
                                   <a target="_blank" href="http://www.facebook.com/sharer/sharer.php?u={{ urlencode($urlToQuestion) }}"><i
